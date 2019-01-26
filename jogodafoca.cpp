09d88f96b999ac6568f46d5a689e6718f2685280
ArtoriasOfAbysss <46583532+ArtoriasOfAbysss@users.noreply.github.com>
2019-01-26 11:03:40 -0200

    Add files via upload

---
+++ jogodafoca.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+
+int main () {
+  char palavra[30],letra[1],secreta[30];
+  int tam,i,chances,acertos;
+  bool acerto;
+
+  chances=6;
+  tam=0;
+  i=0;
+  acerto=false;
+  acertos=0;
+
+  cout << "Fale para o seu amigo tampar os olhos e digite a palavra secreta: " << endl;
+  cin >> palavra;
+  system("cls");
+
+  while(palavra[i] != '\0'){
+    i++;
+    tam++;
+  }
+
+  for(i=0;i<30;i++){
+    secreta[i]='-';
+  }
+
+  while((chances > 0)&&(acertos < tam)){
+    cout << "Chances restantes: " << chances << endl;
+    cout << "Palavra secreta: ";
+    for (i=0; i<tam; i++){
+      cout << secreta[i] << endl;
+    }
+    cout << "Digite uma letra: " << endl;
+    cin >> letra[0];
+    for (i=0; i<tam; i++){
+      if (palavra[i]==letra[0]){
+        acerto=true;
+        secreta[i]=palavra[i];
+        acertos++;
+      }
+    }
+    if(!acerto){
+      chances--;
+    }
+    acerto=false;
+    system("cls");
+  }
+  if (acertos==tam) {
+    cout << "Voce venceu!" << endl;
+  }else{
+    cout << "Que pena, voce perdeu!" << endl;
+  }
+  system ("pause");
+  return 0;
+}
